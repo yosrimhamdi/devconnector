0f7f631f1a2c49956e6f66582082f1efbce318ab
yossri mhamdi <bavary1515@gmail.com>
2020-08-24 13:08:02 +0100

    creating user record on databse.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,9 +2,7 @@ const User = require('../models/user.js');
 const catchAsync = require('../errors/catchAsync');
 
 const signUp = catchAsync(async (req, res) => {
-  const user = await User.create(req.body);
-
-  res.status(200).json({ user: 3 });
+  await User.create(req.body);
 });
 
 module.exports = { signUp };

9d63756459d6221e2a289e08eb739aae503b45d1
yossri mhamdi <bavary1515@gmail.com>
2020-08-24 13:41:31 +0100

    implemented sing up route.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,22 @@
 const User = require('../models/user.js');
 const catchAsync = require('../errors/catchAsync');
+const { createToken } = require('../utils/jwt');
+
+const sendToken = (res, statusCode, payload) => {
+  const token = createToken(payload);
+
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  res.status(statusCode).json({ status: 'succes', message: 'logged In.' });
+};
 
 const signUp = catchAsync(async (req, res) => {
-  await User.create(req.body);
+  const user = await User.create(req.body);
+
+  sendToken(res, 201, { id: user._id });
 });
 
 module.exports = { signUp };

63d4ddf7bf10f42925f7b926f6aa785d683bc460
yossri mhamdi <bavary1515@gmail.com>
2020-08-25 11:05:18 +0100

    removed requireLogin middleware.

--- routers/users.js
+++ routers/users.js
@@ -1,15 +1,11 @@
 const express = require('express');
 
-const { register, logIn, requireLogIn } = require('../controllers/authentication');
+const { register, logIn } = require('../controllers/authentication');
 
 const router = express.Router();
 
 router.route('/auth/register').post(register);
 
 router.route('/auth/login').post(logIn);
-
-router.route('/test').get(requireLogIn, (req, res) => {
-  res.json({ user: req.user, message: 'authorized' });
-});
 
 module.exports = router;

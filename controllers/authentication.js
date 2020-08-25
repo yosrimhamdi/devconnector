b90704569f5276f807fdabb0d82e9117248aa767
yossri mhamdi <bavary1515@gmail.com>
2020-08-25 08:43:50 +0100

    added cookie expiration date.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,11 +3,14 @@ const catchAsync = require('../errors/catchAsync');
 const { createToken } = require('../utils/jwt');
 
 const sendToken = (res, statusCode, payload) => {
+  const { JWT_COOKIE_EXPIRES_IN } = process.env;
+
   const token = createToken(payload);
 
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
+    expires: new Date(Date.now() + JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
   });
 
   res.status(statusCode).json({ status: 'succes', message: 'logged In.' });
--- errors/globalErrorHandler.js
+++ errors/globalErrorHandler.js
@@ -1,5 +1,3 @@
 module.exports = (err, req, res, next) => {
-  console.log(err);
-
   res.status(400).json({ status: 'fail', err, message: err.message });
 };

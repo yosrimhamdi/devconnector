5ee4059c6fe5dd96c602274d8f21b7fdbd420af5
yossri mhamdi <bavary1515@gmail.com>
2020-08-25 22:54:47 +0100

    added user field on teh profile.

--- controllers/profiles/createProfile.js
+++ controllers/profiles/createProfile.js
@@ -2,7 +2,7 @@ const Profile = require('../../models/Profile');
 const catchAsync = require('../../errors/catchAsync');
 
 module.exports = catchAsync(async (req, res) => {
-  const profile = await Profile.create(req.body);
+  const profile = await Profile.create({ ...req.body, user: req.user._id });
 
   res.status(201).json({ status: 'success', profile });
 });

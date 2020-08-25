861eed65aa9d867c1c1be0179706142ccb46867a
yossri mhamdi <bavary1515@gmail.com>
2020-08-25 09:11:29 +0100

    singular upper case model.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.js');
+const User = require('../models/User.js');
 const catchAsync = require('../errors/catchAsync');
 const { createToken } = require('../utils/jwt');
 

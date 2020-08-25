2d14ad4cff2f6f9e3c9c692500a8d273d32936ac
yossri mhamdi <bavary1515@gmail.com>
2020-08-25 15:55:33 +0100

    generating an app error when no profile is found.

--- controllers/profiles/getProfile.js
+++ controllers/profiles/getProfile.js
@@ -1,8 +1,13 @@
 const Profile = require('../../models/Profile');
 const catchAsync = require('../../errors/catchAsync');
+const AppError = require('../../errors/appError');
 
-const getProfile = catchAsync(async (req, res) => {
+const getProfile = catchAsync(async (req, res, next) => {
   const profile = await Profile.findOne({ user: req.user._id });
+
+  if (!profile) {
+    return next(new AppError('no profile yet.', 404));
+  }
 
   res.status(200).json({ status: 'success', profile });
 });

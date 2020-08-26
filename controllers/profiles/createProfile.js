1eefbf8cbf8c3c296c62f09736e2a170efae30c9
yossri mhamdi <bavary1515@gmail.com>
2020-08-26 21:38:54 +0100

    allowing one time profile creation.

--- controllers/profiles/createProfile.js
+++ controllers/profiles/createProfile.js
@@ -1,8 +1,15 @@
 const Profile = require('../../models/Profile');
 const catchAsync = require('../../errors/catchAsync');
+const AppError = require('../../errors/appError');
 
-module.exports = catchAsync(async (req, res) => {
-  const profile = await Profile.create({ ...req.body, user: req.user._id });
+module.exports = catchAsync(async (req, res, next) => {
+  const { _id: id } = req.user;
+
+  if (await Profile.findOne({ user: id })) {
+    return next(new AppError('already has a profile.', 401));
+  }
+
+  const profile = await Profile.create({ ...req.body, user: id });
 
   res.status(201).json({ status: 'success', profile });
 });

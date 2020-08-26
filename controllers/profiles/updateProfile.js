01e4706733b0d24c9801bb2738b8a2500c144ae5
yossri mhamdi <bavary1515@gmail.com>
2020-08-26 09:27:45 +0100

    running validator on update.

--- controllers/profiles/updateProfile.js
+++ controllers/profiles/updateProfile.js
@@ -3,6 +3,7 @@ const Profile = require('../../models/Profile');
 module.exports = async (req, res) => {
   const profile = await Profile.findOneAndUpdate({ user: req.user._id }, req.body, {
     new: true,
+    runValidators: true,
   });
 
   res.status(200).json({ status: 'success', profile });

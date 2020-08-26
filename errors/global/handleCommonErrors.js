8009241d0322480b5370365b719319e52f8d5068
yossri mhamdi <bavary1515@gmail.com>
2020-08-26 10:20:58 +0100

    improved error handling code.

--- errors/global/handleCommonErrors.js
+++ errors/global/handleCommonErrors.js
@@ -1,22 +1,30 @@
 const AppError = require('../appError');
 
 module.exports = (err, req, res, next) => {
-  const { message, name, code } = err;
+  const { message, name } = err;
 
-  if (code === 11000) {
-    const { user, email, handle } = err.keyPattern;
+  switch (name) {
+    case 'MongoError': {
+      const { user, email, handle } = err.keyPattern;
 
-    if (user) {
-      err = new AppError('already has a profile.', 403);
-    } else if (email) {
-      err = new AppError(`email: ${err.keyValue.email} already registered.`, 400);
-    } else if (handle) {
-      err = new AppError(`handle: ${err.keyValue.handle} already exists.`, 400);
+      if (user) {
+        err = new AppError('already has a profile.', 403);
+      } else if (email) {
+        err = new AppError(`email: ${err.keyValue.email} already registered.`, 400);
+      } else if (handle) {
+        err = new AppError(`handle: ${err.keyValue.handle} already exists.`, 400);
+      }
+
+      break;
     }
-  }
 
-  if (name === 'ValidationError') {
-    err = new AppError(message.split('failed: ')[1], 400);
+    case 'ValidationError': {
+      err = new AppError(message.split('failed: ')[1], 400);
+
+      break;
+    }
+
+    default:
   }
 
   switch (message) {

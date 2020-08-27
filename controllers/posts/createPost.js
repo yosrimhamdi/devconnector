c28106f2ece7db9934c0430bba4d78714901e6c6
yossri mhamdi <bavary1515@gmail.com>
2020-08-27 14:36:35 +0100

    using a createPost machine.

--- controllers/posts/createPost.js
+++ controllers/posts/createPost.js
@@ -1,8 +1,4 @@
-const catchAsync = require('../../errors/catchAsync');
+const machine = require('../machine');
 const Post = require('../../models/Post');
 
-module.exports = catchAsync(async (req, res) => {
-  const post = await Post.create({ ...req.body, user: req.user._id });
-
-  res.status(200).json({ status: 'success', post });
-});
+module.exports = machine.createOne(Post);

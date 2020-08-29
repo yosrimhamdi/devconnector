4cd6ba1e3a7b0db13d530e7b47bcf8aaebd844b5
yossri mhamdi <bavary1515@gmail.com>
2020-08-29 09:09:19 +0100

    improved remove like route handler.

--- controllers/posts/likes/removeLike.js
+++ controllers/posts/likes/removeLike.js
@@ -1,12 +1,21 @@
 const catchAsync = require('../../../errors/catchAsync');
-
 const PostLike = require('../../../models/PostLike');
+const AppError = require('../../../errors/appError');
 
-module.exports = catchAsync(async (req, res) => {
-  await PostLike.findOneAndDelete({
+module.exports = catchAsync(async (req, res, next) => {
+  const like = await PostLike.findOneAndDelete({
     user: req.user._id,
     post: req.params.id,
   });
+
+  if (!like) {
+    return next(
+      new AppError(
+        `you didn't make a like on the post with id: ${req.params.id}`,
+        404
+      )
+    );
+  }
 
   res.status(204).json({ status: 'success', like: null });
 });

4474bb1db5870ea0100724d8bcf8053930b9c233
yossri mhamdi <bavary1515@gmail.com>
2020-08-29 12:05:41 +0100

    setting up eslint clien.

--- client/src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <div>Aapp</div>;
-};
-
-export default App;
---
+++ client/src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+
+import './global.scss';
+
+const App = () => {
+  return <div className="app">App</div>;
+};
+
+export default App;

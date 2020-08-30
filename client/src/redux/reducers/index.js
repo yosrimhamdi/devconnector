37eef44207ce71ee78a0e03c435656e8897c0986
yossri mhamdi <bavary1515@gmail.com>
2020-08-30 16:40:13 +0100

    wiring up redux form reducer.

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 import profiles from './profiles';
 
-export default combineReducers({ profiles });
+export default combineReducers({ profiles, form: reduxFormReducer });

a99ac8576b4694fd2dc53b84e5f284952a4b05dc
yossri mhamdi <bavary1515@gmail.com>
2020-08-31 14:01:45 +0100

    added space under the fixed header.

--- client/src/components/pages/Profiles/Profiles.js
+++ client/src/components/pages/Profiles/Profiles.js
@@ -4,18 +4,14 @@ import './Profiles.scss';
 import ProfileList from './ProfileList';
 import Headline from '../../Headline';
 
-class Profiles extends React.Component {
-  render() {
-    return (
-      <div>
-        <Headline
-          header="developer profiles"
-          subHeader="Browse and connect with developers."
-        />
-        <ProfileList />
-      </div>
-    );
-  }
-}
+const Profiles = () => (
+  <div className="profiles">
+    <Headline
+      header="developer profiles"
+      subHeader="Browse and connect with developers."
+    />
+    <ProfileList />
+  </div>
+);
 
 export default Profiles;

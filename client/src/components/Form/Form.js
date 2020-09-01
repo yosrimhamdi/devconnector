2ffcbbd32cd375fe887ae612c941c0f68ff0515c
yossri mhamdi <bavary1515@gmail.com>
2020-09-01 21:35:13 +0100

    back to previous state.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import classnames from 'classnames';
+import { Field } from 'redux-form';
+import { connect } from 'react-redux';
 
 import './Form.scss';
+import Headline from '../Headline';
 
 class Form extends React.Component {
   renderMessage(message) {
@@ -24,7 +27,7 @@ class Form extends React.Component {
     );
   }
 
-  renderResponseError(errorName) {
+  renderResponseError(errorName = '') {
     const error = this.props.errors[errorName];
 
     if (!error) {
@@ -38,7 +41,7 @@ class Form extends React.Component {
     );
   }
 
-  renderInput({ input, type, placeholder, message, errorName, meta }) {
+  renderInput = ({ input, type, placeholder, message, errorName, meta }) => {
     const className = classnames('form__input', {
       'form__input--error': meta.error && meta.touched && !meta.active,
     });
@@ -56,7 +59,40 @@ class Form extends React.Component {
         {this.renderResponseError(errorName)}
       </div>
     );
+  };
+
+  renderFields() {
+    return this.props.fields.map(
+      ({ name, placeholder, type, message, errorName }, i) => (
+        <Field
+          key={i}
+          name={name}
+          errorName={errorName}
+          placeholder={placeholder}
+          message={message}
+          type={type}
+          component={this.renderInput}
+        />
+      ),
+    );
+  }
+
+  render() {
+    const renderedFields = this.renderFields();
+    return (
+      <div className="form">
+        <Headline header={this.props.header} subHeader={this.props.subHeader} />
+        <form noValidate onSubmit={this.props.handleSubmit(this.props.onFormSubmit)}>
+          {renderedFields}
+          <button className="form__button" type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Form;
+const mapStateToProps = ({ errors }) => ({ errors });
+
+export default connect(mapStateToProps)(Form);
--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,98 +1,29 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import classnames from 'classnames';
 
 import './Login.scss';
+import fields from './fields';
+import Form from '../../Form';
 import validate from './validate';
-import Headline from '../../Headline';
 import { loginUser } from '../../../redux/actions';
 
 class Login extends React.Component {
-  renderMessage(message) {
-    if (!message) {
-      return null;
-    }
-
-    return <div className="form__input-message">{message}</div>;
-  }
-
-  renderValidationError({ error, touched, active }) {
-    if (!(touched && !active && error)) {
-      return null;
-    }
-
-    return (
-      <div className="form__input-error-message">
-        <i className="fas fa-exclamation-circle" /> {error}
-      </div>
-    );
-  }
-
-  renderResponseError(error) {
-    if (!error) {
-      return null;
-    }
-
-    return (
-      <div className="form__input-error-message">
-        <i className="fas fa-exclamation-circle" /> {error}
-      </div>
-    );
-  }
-
-  renderInput = ({ input, type, placeholder, message, errorName, meta }) => {
-    const error = this.props.errors[errorName];
-
-    const className = classnames('form__input', {
-      'form__input--error': error || (meta.error && meta.touched && !meta.active),
-    });
-
-    return (
-      <div>
-        <input
-          className={className}
-          placeholder={placeholder}
-          {...input}
-          type={type}
-        />
-        {this.renderMessage(message)}
-        {this.renderValidationError(meta)}
-        {this.renderResponseError(error)}
-      </div>
-    );
-  };
   onFormSubmit = formValues => {
     this.props.loginUser(formValues);
   };
 
   render() {
     return (
-      <div className="form">
-        <Headline
-          header="Log In"
-          subHeader="Sign in to your DevConnector account."
-        />
-        <form noValidate onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
-          <Field
-            name="email"
-            placeholder="email"
-            type="email"
-            component={this.renderInput}
-          />
-          {this.props.errors['wrongemailorpassword']}
-          <Field
-            name="password"
-            placeholder="password"
-            type="password"
-            component={this.renderInput}
-            errorName="wrongemailorpassword"
-          />
-          <button className="form__button" type="submit">
-            submit
-          </button>
-        </form>
-      </div>
+      <Form
+        fields={fields}
+        onFormSubmit={this.onFormSubmit}
+        handleSubmit={this.props.handleSubmit}
+        header="Log In
+        "
+        subHeader="Sign in to your DevConnector account."
+        errors={this.props.errors}
+      />
     );
   }
 }

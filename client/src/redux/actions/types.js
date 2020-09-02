e5e38f836e0c0ce8893f61bb6a44d8a916c1fc26
yossri mhamdi <bavary1515@gmail.com>
2020-09-02 01:08:24 +0100

    refactored types.js file.

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,6 +1,6 @@
 export const FETCH_PROFILES = 'FETCH_PROFILES';
-export const REGISTER_USER = 'REGISTER_USER';
 
+export const REGISTER_USER = 'REGISTER_USER';
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
 

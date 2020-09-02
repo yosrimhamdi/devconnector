90d4422893f861b53ac9bfb76193461a2c591189
yossri mhamdi <bavary1515@gmail.com>
2020-09-02 01:08:02 +0100

    removed an action type.

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,6 +1,7 @@
 export const FETCH_PROFILES = 'FETCH_PROFILES';
 export const REGISTER_USER = 'REGISTER_USER';
+
 export const LOGIN_USER = 'LOGIN_USER';
-export const ERROR_HAS_OCCURED = 'ERROR_HAS_OCCURED';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const LOGOUT_USER = 'LOGOUT_USER';
+
+export const ERROR_HAS_OCCURED = 'ERROR_HAS_OCCURED';

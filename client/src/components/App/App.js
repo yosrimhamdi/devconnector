6391273cfd83f9093881235e499aa8627c3e9924
yossri mhamdi <bavary1515@gmail.com>
2020-09-03 11:54:04 +0100

    create profile component boiler plate.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import history from '../../history';
 
@@ -12,17 +12,21 @@ import ShowProfile from '../pages/ShowProfile';
 import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
+import CreateProfile from '../pages/CreateProfile';
 
 const App = () => (
   <div>
     <Router history={history}>
       <Header />
-      <Route path="/" exact component={Landing} />
-      <Route path="/profiles" exact component={Profiles} />
-      <Route path="/profiles/:handle" exact component={ShowProfile} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/dashboard" exact component={Dashboard} />
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/profiles" exact component={Profiles} />
+        <Route path="/profiles/:handle" exact component={ShowProfile} />
+        <Route path="/profiles/new" exact component={CreateProfile} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/dashboard" exact component={Dashboard} />
+      </Switch>
     </Router>
   </div>
 );
---
+++ client/src/components/pages/CreateProfile/CreateProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreateProfile extends React.Component {
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  render() {
+    return <div>CreateProfile</div>;
+  }
+}
+
+export default CreateProfile;

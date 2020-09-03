db218c2324482ab492d43cc6126a07efb2fe05eb
yossri mhamdi <bavary1515@gmail.com>
2020-09-03 09:23:53 +0100

    renamed a folder.

--- client/src/components/pages/Dashboard/Experiences/Experiences.js
+++ client/src/components/pages/Dashboard/Experiences/Experiences.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ExperienceItem from './ExpItem';
+import ExperienceItem from './ExperienceItem';
 import './Experiences.scss';
 
 const Experiences = ({ experiences }) => {

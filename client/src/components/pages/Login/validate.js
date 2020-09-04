d547efbcf157f5aed1a4ce90553325561ee204be
yossri mhamdi <bavary1515@gmail.com>
2020-09-04 14:13:15 +0100

    improved Login form validation.

--- client/src/components/pages/Login/validate.js
+++ client/src/components/pages/Login/validate.js
@@ -1,14 +1,14 @@
 import validator from 'validator';
 
-export default ({ email = '', password }) => {
+export default ({ email = '', password = '' }) => {
   const err = {};
 
   if (!validator.isEmail(email)) {
     err.email = 'invalid email';
   }
 
-  if (!password) {
-    err.password = 'password is required';
+  if (!validator.isLength(password, { min: 6, max: 30 })) {
+    err.password = 'password in required: between 6 and 30 characters';
   }
 
   return err;

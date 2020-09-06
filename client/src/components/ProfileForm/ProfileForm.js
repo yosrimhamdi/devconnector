a64c6c616d380d8cef2f203c88138c6403c85790
yossri mhamdi <bavary1515@gmail.com>
2020-09-06 23:26:25 +0100

    splitting skills to array in the front-end.

--- client/src/components/ProfileForm/ProfileForm.js
+++ client/src/components/ProfileForm/ProfileForm.js
@@ -6,83 +6,91 @@ import Headline from '../Headline';
 import { Input, TextArea, Select } from '../form';
 import CostumLink from '../CostumLink';
 
-const ProfileForm = ({
-  handleSubmit,
-  onFormSubmit,
-  errors,
-  header,
-  subHeader,
-}) => (
-  <div>
-    <Headline header={header} subHeader={subHeader} />
-    <form onSubmit={handleSubmit(onFormSubmit)} className="form">
-      <CostumLink
-        to="/dashboard"
-        text="go back"
-        bgColor="rgb(236, 235, 235)"
-        color="black"
-      />
-      <div className="form__require-message">* = required</div>
-      <Field
-        name="handle"
-        placeholder="* profile handle"
-        message="a unique handle for your profile URL. Your full name, company name, nickname"
-        type="text"
-        component={Input}
-        responseError={errors.handleExists}
-      />
-      <Field
-        name="status"
-        message="give us an idea of where you are at in your career"
-        component={Select}
-      />
-      <Field
-        name="compmany"
-        placeholder="compnay"
-        type="text"
-        message="could be your own company or one you work for"
-        component={Input}
-      />
-      <Field
-        name="website"
-        placeholder="website"
-        type="text"
-        message="could be your own website or company one"
-        component={Input}
-      />
-      <Field
-        name="location"
-        placeholder="location"
-        type="text"
-        message="city or city & state suggested (eg. Boston, MA"
-        component={Input}
-      />
-      <Field
-        name="skills"
-        placeholder="* skills"
-        type="text"
-        message="please use comma, separated values (eg. HTML, CSS, PHP)"
-        component={Input}
-      />
-      <Field
-        name="githubUserName"
-        placeholder="github username"
-        type="text"
-        message="if you want your latest repos and a Github link, include your username"
-        component={Input}
-      />
-      <Field
-        name="bio"
-        placeholder="short bio"
-        message="tell us a little about yourself"
-        component={TextArea}
-      />
-      <button className="form__button" type="submit">
-        submit
-      </button>
-    </form>
-  </div>
-);
+class ProfileForm extends React.Component {
+  onFormSubmit = formValues => {
+    const profile = formValues;
+
+    profile.skills = profile.skills.split(',');
+
+    this.props.onFormSubmit(profile);
+  };
+
+  render() {
+    const { handleSubmit, errors, header, subHeader } = this.props;
+
+    return (
+      <div>
+        <Headline header={header} subHeader={subHeader} />
+        <form onSubmit={handleSubmit(this.onFormSubmit)} className="form">
+          <CostumLink
+            to="/dashboard"
+            text="go back"
+            bgColor="rgb(236, 235, 235)"
+            color="black"
+          />
+          <div className="form__require-message">* = required</div>
+          <Field
+            name="handle"
+            placeholder="* profile handle"
+            message="a unique handle for your profile URL. Your full name, company name, nickname"
+            type="text"
+            component={Input}
+            responseError={errors.handleExists}
+          />
+          <Field
+            name="status"
+            message="give us an idea of where you are at in your career"
+            component={Select}
+          />
+          <Field
+            name="compmany"
+            placeholder="compnay"
+            type="text"
+            message="could be your own company or one you work for"
+            component={Input}
+          />
+          <Field
+            name="website"
+            placeholder="website"
+            type="text"
+            message="could be your own website or company one"
+            component={Input}
+          />
+          <Field
+            name="location"
+            placeholder="location"
+            type="text"
+            message="city or city & state suggested (eg. Boston, MA"
+            component={Input}
+          />
+          <Field
+            name="skills"
+            placeholder="* skills"
+            type="text"
+            message="please use comma, separated values (eg. HTML, CSS, PHP)"
+            component={Input}
+          />
+          <Field
+            name="githubUserName"
+            placeholder="github username"
+            type="text"
+            message="if you want your latest repos and a Github link, include your username"
+            component={Input}
+          />
+          <Field
+            name="bio"
+            placeholder="short bio"
+            message="tell us a little about yourself"
+            component={TextArea}
+          />
+          <button className="form__button" type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ errors }) => ({ errors });
 

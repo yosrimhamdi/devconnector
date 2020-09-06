fa61568c70d364da12560d335a7626171970e604
yossri mhamdi <bavary1515@gmail.com>
2020-09-06 23:13:55 +0100

    improved the Headline styling.

--- client/src/components/Headline/Headline.js
+++ client/src/components/Headline/Headline.js
@@ -5,7 +5,7 @@ import './Headline.scss';
 const Headline = ({ header, subHeader }) => (
   <div className="headline">
     <h1 className="headline__title">{header}</h1>
-    <h3 className="headline__message">{subHeader}</h3>
+    <h3 className="headline__sub-title">{subHeader}</h3>
   </div>
 );
 

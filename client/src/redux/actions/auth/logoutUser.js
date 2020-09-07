288818226d312fcd85baae71d57f3c9a0fae964d
yossri mhamdi <bavary1515@gmail.com>
2020-09-07 18:01:39 +0100

    we do not need history.push in logoutUser action creator.

--- client/src/redux/actions/auth/logoutUser.js
+++ client/src/redux/actions/auth/logoutUser.js
@@ -1,14 +1,11 @@
 import { LOGOUT_USER, ERROR_HAS_OCCURED } from '../types';
 import users from '../../../apis/users';
-import history from '../../../history';
 
 export default () => async dispatch => {
   try {
     await users.get('/logout');
 
     dispatch({ type: LOGOUT_USER });
-
-    history.push('/login');
   } catch (err) {
     dispatch({
       type: ERROR_HAS_OCCURED,

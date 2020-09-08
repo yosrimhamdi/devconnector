9b63b9c8343aa459972beae3d0b9750ea107ac0e
yossri mhamdi <bavary1515@gmail.com>
2020-09-08 10:40:59 +0100

    added redux store into a seperate file.

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
-import reducers from './redux/reducers';
+import store from './redux/store';
 import establishAuth from './utils/establishAuth';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk)),
-);
 
 establishAuth(store);
 

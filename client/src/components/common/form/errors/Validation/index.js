dac118d8078285d6e2c52f9e0bd4b7d0616ede1b
yossri mhamdi <bavary1515@gmail.com>
2020-09-08 17:30:28 +0100

    added an svg.

--- client/src/components/common/form/errors/Validation/index.js
+++ client/src/components/common/form/errors/Validation/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import errorIcon from '../error.svg';
+
 export default ({ meta }) => {
   const { error, touched, active } = meta;
 
   if (touched && !active && error) {
     return (
       <div className="form__input-error-message">
-        <i className="fas fa-exclamation-circle" /> {error}.
+        <img src={errorIcon} className="form__error-icon" alt="error" /> {error}
+        .
       </div>
     );
   }

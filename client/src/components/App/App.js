a59c559efd969953511a353e61fbb16050508183
yossri mhamdi <bavary1515@gmail.com>
2020-09-08 10:49:07 +0100

    added a NotFoundPage component.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import history from '../../history';
+import { PrivateRoute } from '../common/Routes';
 
 import './global.scss';
-import Header from '../layouts/Header';
 
+import Header from '../layouts/Header';
 import Landing from '../pages/Landing';
 import Profiles from '../pages/Profiles';
 import Register from '../pages/Register';
@@ -15,9 +16,9 @@ import CreateProfile from '../pages/CreateProfile';
 import UpdateProfile from '../pages/UpdateProfile';
 import AddExperience from '../pages/AddExperience';
 import AddEducation from '../pages/AddEducation';
-import { PrivateRoute } from '../common/Routes';
+import NotFoundPage from '../pages/NotFoundPage';
 
-const NonLanding = () => (
+const Others = () => (
   <div style={{ margin: '6em 0' }}>
     <Switch>
       <PrivateRoute path="/dashboard" exact component={Dashboard} />
@@ -28,7 +29,7 @@ const NonLanding = () => (
       <Route path="/profiles/new" exact component={CreateProfile} />
       <Route path="/profiles/experience/new" exact component={AddExperience} />
       <Route path="/profiles/education/new" exact component={AddEducation} />
-      <Route path="*" exact component={() => <div>not found</div>} />
+      <Route path="*" component={NotFoundPage} />
     </Switch>
   </div>
 );
@@ -38,7 +39,7 @@ const App = () => (
     <Header />
     <Switch>
       <Route path="/" exact component={Landing} />
-      <Route component={NonLanding} />
+      <Route path="*" component={Others} />
     </Switch>
   </Router>
 );

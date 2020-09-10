c1c29176d4567f2787869b41c300b415fafbfe57
yossri mhamdi <bavary1515@gmail.com>
2020-09-10 14:34:22 +0100

    [fix] form create post validation.

--- client/src/components/pages/Posts/validate.js
+++ client/src/components/pages/Posts/validate.js
@@ -3,7 +3,7 @@ import { isLength } from 'validator';
 export default ({ text = '' }) => {
   const error = {};
 
-  if (!isLength(text, { min: 10, max: 300 })) {
+  if (text && !isLength(text, { min: 10, max: 300 })) {
     error.text = 'post must be between 10 and 300 characters';
   }
 

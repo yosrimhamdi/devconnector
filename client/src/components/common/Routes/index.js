8311213bd25da26934e9dee3d5b682e37dff30a2
yossri mhamdi <bavary1515@gmail.com>
2020-09-10 19:51:35 +0100

    removed a route.

--- client/src/components/common/Routes/index.js
+++ client/src/components/common/Routes/index.js
@@ -1,4 +1,3 @@
 export { default as PrivateRoute } from './PrivateRoute';
 export { default as RestrictedRoute } from './RestrictedRoute';
 export { default as RestrictedRequireProfileRoute } from './RestrictedRequireProfileRoute';
-export { default as RestrictedRequireNoProfileRoute } from './RestrictedRequireNoProfileRoute';

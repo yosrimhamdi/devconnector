4162cf6d3bd6a86eb126dea2ae4ead10186395a6
yossri mhamdi <bavary1515@gmail.com>
2020-09-11 11:14:01 +0100

    improved component readabislity.

--- client/src/components/layouts/Footer/Footer.js
+++ client/src/components/layouts/Footer/Footer.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './Footer.scss';
 
-const Footer = () => {
-  return (
-    <div className="footer">
-      Copyright &copy; {new Date().getFullYear()} Dev Connector
-    </div>
-  );
-};
+const Footer = () => (
+  <div className="footer">
+    Copyright &copy; {new Date().getFullYear()} Dev Connector
+  </div>
+);
 
 export default Footer;

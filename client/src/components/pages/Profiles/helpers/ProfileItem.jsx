9b8047067d38a2359ee6b15205c0b5ff5014e5cc
yossri mhamdi <bavary1515@gmail.com>
2020-09-17 13:21:17 +0100

    turned profiles item into good looding cards.

--- client/src/components/pages/Profiles/helpers/ProfileItem.jsx
+++ client/src/components/pages/Profiles/helpers/ProfileItem.jsx
@@ -1,38 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import moment from 'moment';
 
 import './ProfileItem.scss';
 import Company from '../../../common/Company';
-import getUserFirstName from '../../../../utils/getUserFirstName';
 
 const ProfileItem = ({ profile }) => {
-  const { user, handle, status, company, createdAt } = profile;
+  const { user, handle, status, company } = profile;
 
   return (
     <li className="profile-item">
       <img
         className="profile-item__image"
-        src={`http://localhost:3001/images/${user.photo}`}
+        src={`/images/${user.photo}`}
         alt={`${user.name}'s user avatar`}
       />
-      <div className="profile-item__details">
-        <div className="profile-item__user-name">
-          {getUserFirstName(user.name)}
-        </div>
-        <div className="profile-item__date">
-          joined {moment(createdAt, 'YYYYMMDD').fromNow()}
-        </div>
-        <div className="profile-item__user-job">
-          <span className="profile-item__user-status">{status}</span>
-          <Company company={company} />
-        </div>
+      <div className="profile-item__user-name">{user.name}</div>
+      <div className="profile-item__user-job">
+        <span className="profile-item__user-status">{status}</span>
+        <Company company={company} />
       </div>
-      <div className="profile-item__button-wrapper">
-        <Link to={`/profiles/${handle}`} className="profile-item__button">
-          view profile
-        </Link>
-      </div>
+      <Link to={`/profiles/${handle}`} className="profile-item__button">
+        view profile
+      </Link>
     </li>
   );
 };

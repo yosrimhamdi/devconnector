778f1bf4afe3383a1b143aa52351ed2cf96bb983
yossri mhamdi <bavary1515@gmail.com>
2020-09-17 13:50:31 +0100

    fixed header overlaping issue.

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -24,43 +24,37 @@ import Profile from '../pages/Profile';
 import Posts from '../pages/Posts';
 import NotFound from '../pages/NotFound';
 
-const Others = () => (
-  <div style={{ padding: '6em 0' }}>
-    <Switch>
-      <PrivateRoute path="/dashboard" exact component={Dashboard} />
-      <RestrictedRoute path="/register" exact component={Register} />
-      <RestrictedRoute path="/login" exact component={Login} />
-      <Route path="/profiles" exact component={Profiles} />
-      <Route path="/profiles/new" exact component={CreateProfile} />
-      <RestrictedRequireProfileRoute
-        path="/profiles/update"
-        exact
-        component={UpdateProfile}
-      />
-      <RestrictedRequireProfileRoute
-        path="/profiles/experience/new"
-        exact
-        component={AddExperience}
-      />
-      <RestrictedRequireProfileRoute
-        path="/profiles/education/new"
-        exact
-        component={AddEducation}
-      />
-      <Route path="/profiles/:handle" exact component={Profile} />
-      <PrivateRoute path="/posts" exact component={Posts} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  </div>
-);
-
 const App = () => (
   <Router history={history}>
     <Header />
-    <Switch>
-      <Route path="/" exact component={Landing} />
-      <Route path="*" component={Others} />
-    </Switch>
+    <div className="content-wrapper">
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <PrivateRoute path="/dashboard" exact component={Dashboard} />
+        <RestrictedRoute path="/register" exact component={Register} />
+        <RestrictedRoute path="/login" exact component={Login} />
+        <Route path="/profiles" exact component={Profiles} />
+        <Route path="/profiles/new" exact component={CreateProfile} />
+        <RestrictedRequireProfileRoute
+          path="/profiles/update"
+          exact
+          component={UpdateProfile}
+        />
+        <RestrictedRequireProfileRoute
+          path="/profiles/experience/new"
+          exact
+          component={AddExperience}
+        />
+        <RestrictedRequireProfileRoute
+          path="/profiles/education/new"
+          exact
+          component={AddEducation}
+        />
+        <Route path="/profiles/:handle" exact component={Profile} />
+        <PrivateRoute path="/posts" exact component={Posts} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </div>
   </Router>
 );
 

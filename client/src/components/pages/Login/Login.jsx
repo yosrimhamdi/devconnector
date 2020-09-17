13f50b644a8cd97449d78e705eeb13d7229e22fa
yossri mhamdi <bavary1515@gmail.com>
2020-09-17 21:02:53 +0100

    improved spacing.

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -2,36 +2,38 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 
+import './Login.scss';
 import validate from './validate';
 import { loginUser } from '../../../redux/actions';
 import Headline from '../../common/Headline';
-
 import { Input } from '../../common/form';
 
 const Login = ({ handleSubmit, loginUser, errors }) => (
-  <div>
-    <Headline
-      header="log in"
-      subHeader="sign in to your DevConnector account."
-    />
-    <form onSubmit={handleSubmit(loginUser)} className="form">
-      <Field
-        name="email"
-        placeholder="email"
-        component={Input}
-        responseError={errors.wrongEmailOrPassword}
+  <div className="login">
+    <div className="login__content">
+      <Headline
+        header="log in"
+        subHeader="sign in to your DevConnector account"
       />
-      <Field
-        name="password"
-        type="password"
-        placeholder="password"
-        component={Input}
-        responseError={errors.wrongEmailOrPassword}
-      />
-      <button className="form__submit-btn" type="submit">
-        submit
-      </button>
-    </form>
+      <form onSubmit={handleSubmit(loginUser)} className="form">
+        <Field
+          name="email"
+          placeholder="email"
+          component={Input}
+          responseError={errors.wrongEmailOrPassword}
+        />
+        <Field
+          name="password"
+          type="password"
+          placeholder="password"
+          component={Input}
+          responseError={errors.wrongEmailOrPassword}
+        />
+        <button className="form__submit-btn" type="submit">
+          submit
+        </button>
+      </form>
+    </div>
   </div>
 );
 
--- client/src/components/pages/Register/Register.jsx
+++ client/src/components/pages/Register/Register.jsx
@@ -2,38 +2,41 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 
+import './Register.scss';
 import validate from './validate';
 import { Input } from '../../common/form';
 import Headline from '../../common/Headline';
 import { registerUser } from '../../../redux/actions';
 
 const Register = ({ handleSubmit, registerUser, errors }) => (
-  <div>
-    <Headline header="sign up" subHeader="create your DevConnector account." />
-    <form onSubmit={handleSubmit(registerUser)} className="form">
-      <Field name="name" placeholder="name" component={Input} />
-      <Field
-        name="email"
-        placeholder="email"
-        component={Input}
-        responseError={errors.emailExists}
-      />
-      <Field
-        name="password"
-        type="password"
-        placeholder="password"
-        component={Input}
-      />
-      <Field
-        name="passwordConfirm"
-        type="password"
-        placeholder="confirm password"
-        component={Input}
-      />
-      <button className="form__submit-btn" type="submit">
-        submit
-      </button>
-    </form>
+  <div className="register">
+    <div className="register__content">
+      <Headline header="sign up" subHeader="create your DevConnector account" />
+      <form onSubmit={handleSubmit(registerUser)} className="form">
+        <Field name="name" placeholder="name" component={Input} />
+        <Field
+          name="email"
+          placeholder="email"
+          component={Input}
+          responseError={errors.emailExists}
+        />
+        <Field
+          name="password"
+          type="password"
+          placeholder="password"
+          component={Input}
+        />
+        <Field
+          name="passwordConfirm"
+          type="password"
+          placeholder="confirm password"
+          component={Input}
+        />
+        <button className="form__submit-btn" type="submit">
+          submit
+        </button>
+      </form>
+    </div>
   </div>
 );
 

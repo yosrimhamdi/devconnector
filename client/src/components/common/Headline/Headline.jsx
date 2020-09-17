9de881ccf1291b60d1fe0e68628d430e7c66b287
yossri mhamdi <bavary1515@gmail.com>
2020-09-17 13:35:23 +0100

    changed the styling of the Headline component.

--- client/src/components/common/Headline/Headline.jsx
+++ client/src/components/common/Headline/Headline.jsx
@@ -4,8 +4,10 @@ import './Headline.scss';
 
 const Headline = ({ header, subHeader }) => (
   <div className="headline">
-    <h1 className="headline__title">{header}</h1>
-    <h3 className="headline__sub-title">{subHeader}</h3>
+    <div className="headline__wrapper">
+      <h1 className="headline__title">{header}</h1>
+      <h3 className="headline__sub-title">{subHeader}</h3>
+    </div>
   </div>
 );
 

157fe0560d4c92318046f4ec0fd92c92fbdf6aad
yossri mhamdi <bavary1515@gmail.com>
2020-09-18 23:36:02 +0100

    changed the profiles background-color && removed the headlines.

--- client/src/components/pages/Profiles/Profiles.jsx
+++ client/src/components/pages/Profiles/Profiles.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 import './Profiles.scss';
 import ProfileList from './helpers/ProfileList';
-import Headline from '../../common/Headline';
 
 const Profiles = () => (
   <div className="profiles content-wrapper">
-    <Headline
-      header="developer profiles"
-      subHeader="Browse and connect with developers"
-    />
     <ProfileList />
   </div>
 );

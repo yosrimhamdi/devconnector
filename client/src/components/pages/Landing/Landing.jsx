fed7492140b95817a5e64c54b8f45980fb756af9
yossri mhamdi <bavary1515@gmail.com>
2020-09-19 17:22:13 +0100

    fixing the landing headers typo.

--- client/src/components/pages/Landing/Landing.jsx
+++ client/src/components/pages/Landing/Landing.jsx
@@ -6,13 +6,15 @@ import './Landing.scss';
 const Landing = () => (
   <div className="landing">
     <div className="landing__content">
-      <h1 className="landing__header">Developer Connector</h1>
+      <h1 className="landing__header">
+        <span>Developer Connector</span>
+      </h1>
       <h2 className="landing__sub-header">
         Create a developer profile/portfolio, share posts and get help from
         other developers
       </h2>
       <div className="landing__button-container">
-        <Link to="/signup" className="link">
+        <Link to="/register" className="link">
           sign up
         </Link>
         <Link to="/login" className="link link--grey">

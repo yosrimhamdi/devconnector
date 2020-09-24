5ad18e9991cbddd65a9e0eae101fe40bc6fa2d39
yossri mhamdi <bavary1515@gmail.com>
2020-09-24 17:33:16 +0100

    improved pagination styling.

--- client/src/components/common/Pagination/Pagination.jsx
+++ client/src/components/common/Pagination/Pagination.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MaterialPagination from '@material-ui/lab/Pagination';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 
 import profiles from '../../../apis/profiles';
 import { fetchProfiles } from '../../../redux/actions';
@@ -10,12 +11,14 @@ import './Pagination.scss';
 const Pagination = ({ fetchProfiles }) => {
   const [count, setCount] = useState(1);
   const [prevPage, setPrevPage] = useState(1);
+  const [hidden, setHidden] = useState(true);
 
   useEffect(() => {
     const getCount = async () => {
       const response = await profiles.get('/all/count');
 
       setCount(response.data.count);
+      setHidden(false);
     };
 
     getCount();
@@ -28,8 +31,12 @@ const Pagination = ({ fetchProfiles }) => {
     }
   };
 
+  const className = classnames('pagination', {
+    'pagination--hidden': hidden,
+  });
+
   return (
-    <div className="pagination">
+    <div className={className}>
       <MaterialPagination
         count={count}
         color="primary"

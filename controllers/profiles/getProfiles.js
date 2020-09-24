f60609570abd256cb4178e01b388db5d1c047cc5
yossri mhamdi <bavary1515@gmail.com>
2020-09-24 15:04:57 +0100

    extracted a constant into a seperate file.

--- controllers/profiles/getProfiles.js
+++ controllers/profiles/getProfiles.js
@@ -1,12 +1,11 @@
 const Profile = require('../../models/Profile');
 const catchAsync = require('../../errors/catchAsync');
+const PROFILE_PER_PAGE = require('../../utils/PROFILE_PER_PAGE');
 
 module.exports = catchAsync(async (req, res) => {
-  const profilePerPage = 3;
-
   const profiles = await Profile.find()
-    .skip(profilePerPage * (req.query.page - 1))
-    .limit(profilePerPage);
+    .skip(PROFILE_PER_PAGE * (req.query.page - 1))
+    .limit(PROFILE_PER_PAGE);
 
   res.status(200).json({ status: 'success', profiles });
 });
--- controllers/profiles/getProfilesCount.js
+++ controllers/profiles/getProfilesCount.js
@@ -1,10 +1,11 @@
 const catchAsync = require('../../errors/catchAsync');
 const Profile = require('../../models/Profile');
+const PROFILE_PER_AGE = require('../../utils/PROFILE_PER_PAGE');
 
 module.exports = catchAsync(async (req, res, next) => {
   const profiles = await Profile.find();
 
-  const count = Math.ceil(profiles.length / 3);
+  const count = Math.ceil(profiles.length / PROFILE_PER_AGE);
 
   res.status(200).json({ count: count });
 });

1c1d6e1dbb7eb90698066c0e0d4398711fe66f6f
yossri mhamdi <bavary1515@gmail.com>
2020-09-24 19:15:27 +0100

    exact overlap header space fix.

--- client/src/components/layouts/Header/Header.jsx
+++ client/src/components/layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,21 +7,34 @@ import { logoutUser } from '../../../redux/actions';
 
 import Navigation from './Navgitation';
 
-const Header = ({ auth, logoutUser }) => (
-  <div className="header">
-    <div className="header__left-section">
-      <Link to="/" className="header__devconnector">
-        devConnector
-      </Link>
-      <Link to="/profiles" className="header__link">
-        developers
-      </Link>
+const Header = ({ auth, logoutUser }) => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const headerHeight = headerRef.current.clientHeight;
+
+    document.documentElement.style.setProperty(
+      '--header-height',
+      `${headerHeight}px`,
+    );
+  }, []);
+
+  return (
+    <div className="header" ref={headerRef}>
+      <div className="header__left-section">
+        <Link to="/" className="header__devconnector">
+          devConnector
+        </Link>
+        <Link to="/profiles" className="header__link">
+          developers
+        </Link>
+      </div>
+      <div className="header__right-section">
+        <Navigation auth={auth} logoutUser={logoutUser} />
+      </div>
     </div>
-    <div className="header__right-section">
-      <Navigation auth={auth} logoutUser={logoutUser} />
-    </div>
-  </div>
-);
+  );
+};
 
 const mapStateToProps = ({ auth }) => ({ auth });
 

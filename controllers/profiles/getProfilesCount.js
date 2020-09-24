92bfab0ef0f0a4e75f7ffe4ef56f42931a0b994b
yossri mhamdi <bavary1515@gmail.com>
2020-09-24 15:05:46 +0100

    improved code.

--- controllers/profiles/getProfilesCount.js
+++ controllers/profiles/getProfilesCount.js
@@ -2,10 +2,10 @@ const catchAsync = require('../../errors/catchAsync');
 const Profile = require('../../models/Profile');
 const PROFILE_PER_AGE = require('../../utils/PROFILE_PER_PAGE');
 
-module.exports = catchAsync(async (req, res, next) => {
+module.exports = catchAsync(async (req, res) => {
   const profiles = await Profile.find();
 
   const count = Math.ceil(profiles.length / PROFILE_PER_AGE);
 
-  res.status(200).json({ count: count });
+  res.status(200).json({ count });
 });

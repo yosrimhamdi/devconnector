66ef21a5dbbef48966973d146cda6c849a062a5d
yossri mhamdi <bavary1515@gmail.com>
2020-09-26 23:53:19 +0100

    test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-// app.use('/api/users', users);
+app.use('/api/users', users);
 
 app.use('/api/profiles', profiles);
 

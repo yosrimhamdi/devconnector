eb4c6ea05bb0ec0e73fe965508a4287a2073b2da
yossri mhamdi <bavary1515@gmail.com>
2020-09-26 23:08:59 +0100

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-// dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env' });
 
 const app = require('./app');
 

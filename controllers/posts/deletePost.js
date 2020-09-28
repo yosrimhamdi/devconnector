8788fccffde7c6d2fc9bab935ec4f05a7604d466
yossri mhamdi <bavary1515@gmail.com>
2020-09-28 08:59:38 +0100

    deleting comments/likes when a post is deleted.

--- controllers/posts/deletePost.js
+++ controllers/posts/deletePost.js
@@ -1,17 +1,25 @@
 const catchAsync = require('../../errors/catchAsync');
 const Post = require('../../models/Post');
+const Comment = require('../../models/Comment');
+const Like = require('../../models/Like');
 const AppError = require('../../errors/appError');
 
 module.exports = catchAsync(async (req, res, next) => {
-  const post = await Post.findById(req.params.id);
+  const { id: postId } = req.params;
+
+  const post = await Post.findById(postId);
 
   if (post.user._id.toString() !== req.user._id.toString()) {
     return next(
-      new AppError("Aunothorized. you can't delete other peoples's posts", 401)
+      new AppError("Aunothorized. you can't delete other peoples's posts", 401),
     );
   }
 
   await post.remove();
+
+  await Comment.deleteMany({ post: postId });
+
+  await Like.deleteMany({ post: postId });
 
   res.status(204).json({ status: 'success', post: null });
 });
--- makefile
+++ makefile
@@ -2,4 +2,3 @@ git:
 		git add .
 		git commit -m "$m"
 		git push origin master
-		git push heroku master

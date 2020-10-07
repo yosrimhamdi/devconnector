d173bc1ff38140e3ff48957528531a26c648edc8
yossri mhamdi <bavary1515@gmail.com>
2020-10-07 16:47:10 +0100

    removed profiles/ pagination and fetching all profiles at once.

--- client/src/components/pages/Profiles/Profiles.jsx
+++ client/src/components/pages/Profiles/Profiles.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import './Profiles.scss';
 import ProfileList from './helpers/ProfileList';
-import Pagination from '../../common/Pagination';
 
 const Profiles = () => (
   <div className="profiles">
     <ProfileList />
-    <Pagination />
   </div>
 );
 
--- controllers/profiles/getProfiles.js
+++ controllers/profiles/getProfiles.js
@@ -3,9 +3,7 @@ const catchAsync = require('../../errors/catchAsync');
 const PROFILES_PER_PAGE = require('../../utils/PROFILES_PER_PAGE');
 
 module.exports = catchAsync(async (req, res) => {
-  const profiles = await Profile.find()
-    .skip(PROFILES_PER_PAGE * (req.query.page - 1))
-    .limit(PROFILES_PER_PAGE);
+  const profiles = await Profile.find();
 
   res.status(200).json({ status: 'success', profiles });
 });

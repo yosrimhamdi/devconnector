b2b43dd739e6e510ac3ce87e30b89e8125088a5c
yossri mhamdi <bavary1515@gmail.com>
2020-10-07 18:31:52 +0100

    Revert "removed profiles/ pagination and fetching all profiles at once."
    This reverts commit d173bc1ff38140e3ff48957528531a26c648edc8.

--- controllers/profiles/getProfiles.js
+++ controllers/profiles/getProfiles.js
@@ -3,7 +3,9 @@ const catchAsync = require('../../errors/catchAsync');
 const PROFILES_PER_PAGE = require('../../utils/PROFILES_PER_PAGE');
 
 module.exports = catchAsync(async (req, res) => {
-  const profiles = await Profile.find();
+  const profiles = await Profile.find()
+    .skip(PROFILES_PER_PAGE * (req.query.page - 1))
+    .limit(PROFILES_PER_PAGE);
 
   res.status(200).json({ status: 'success', profiles });
 });

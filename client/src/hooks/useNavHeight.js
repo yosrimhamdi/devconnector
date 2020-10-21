60ac8fe3b74e14922eae233d9613648c4777e2a1
yosrimhamdi <bavary1515@gmail.com>
2020-10-21 23:02:46 +0100

    [fix] nav height calculation is not accurate.

--- client/src/hooks/useNavHeight.js
+++ client/src/hooks/useNavHeight.js
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 
 export default navRef => {
   useEffect(() => {
-    const navHeight = navRef.current.offsetHeight;
+    setTimeout(() => {
+      const navHeight = navRef.current.offsetHeight;
 
-    document.documentElement.style.setProperty(
-      '--nav-height',
-      `${navHeight}px`,
-    );
+      document.documentElement.style.setProperty(
+        '--nav-height',
+        `${navHeight}px`,
+      );
+    }, 100);
   }, [navRef]);
 };

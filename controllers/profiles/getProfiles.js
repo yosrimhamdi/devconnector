5e0efebac31fc56d3dd02a3ddd6dfdb29f307f07
yosri mhamdi <bavary1515@gmail.com>
2021-01-04 21:13:56 +0100

    sorting profiles.

--- controllers/profiles/getProfiles.js
+++ controllers/profiles/getProfiles.js
@@ -4,6 +4,7 @@ const PROFILES_PER_PAGE = require('./PROFILES_PER_PAGE');
 
 module.exports = catchAsync(async (req, res) => {
   const profiles = await Profile.find()
+    .sort('createdAt')
     .skip(PROFILES_PER_PAGE * (req.query.page - 1))
     .limit(PROFILES_PER_PAGE);
 

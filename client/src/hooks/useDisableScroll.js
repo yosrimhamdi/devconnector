07fbcb75c08f76686cc193eeb3a0ad5e81a5bdb1
yosri mhamdi <bavary1515@gmail.com>
2021-01-05 22:24:31 +0100

    changed a variable name.

--- client/src/hooks/useDisableScroll.js
+++ client/src/hooks/useDisableScroll.js
@@ -2,14 +2,14 @@ import { useEffect } from 'react';
 
 import '../assets/styles/components/scroll-disabled.scss';
 
-export default loading => {
+export default isModalShown => {
   useEffect(() => {
-    if (loading) {
+    if (isModalShown) {
       document.body.classList.add('scroll-disabled');
     } else {
       document.body.classList.remove('scroll-disabled');
     }
 
     return () => document.body.classList.remove('scroll-disabled');
-  }, [loading]);
+  }, [isModalShown]);
 };

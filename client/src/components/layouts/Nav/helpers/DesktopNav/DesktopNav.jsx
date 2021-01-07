56bff27a2ed470dbba1ca8f91c4dac242da9208c
yosri mhamdi <bavary1515@gmail.com>
2021-01-07 18:30:26 +0100

    changed the nav.

--- client/src/components/layouts/Nav/helpers/DesktopNav/DesktopNav.jsx
+++ client/src/components/layouts/Nav/helpers/DesktopNav/DesktopNav.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { connect } from 'react-redux';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './DesktopNav.scss';
 import logoutUser from '../../../../../redux/actions/auth/logoutUser';
@@ -11,85 +11,40 @@ const DesktopNav = ({ auth: { isAuthenticated, user }, logoutUser }) => {
 
   useNavHeight(navRef);
 
-  let left = (
-    <div className="desktop-nav__section">
-      <Link to="/">devConnector</Link>
-      <NavLink
-        to="/profiles"
-        className="desktop-nav__link"
-        activeClassName="desktop-nav__link--active"
-      >
-        developers
-      </NavLink>
-    </div>
-  );
-
-  let right = (
-    <div className="desktop-nav__section">
-      <NavLink
-        to="/signup"
-        className="desktop-nav__link"
-        activeClassName="desktop-nav__link--active"
-      >
-        sign up
-      </NavLink>
-      <NavLink
-        to="/login"
-        className="desktop-nav__link"
-        activeClassName="desktop-nav__link--active"
-      >
-        login
-      </NavLink>
-    </div>
+  let content = (
+    <>
+      <Link to="/" className="desktop-nav__devconnector">
+        microsoft
+      </Link>
+      <Link to="/login" className="desktop-nav__login-button">
+        sign in
+      </Link>
+    </>
   );
 
   if (isAuthenticated) {
-    left = (
-      <div className="desktop-nav__section">
-        <NavLink
-          to="/profiles"
-          className="desktop-nav__link"
-          activeClassName="desktop-nav__link--active"
-        >
-          developers
-        </NavLink>
-        <NavLink
-          to="/posts"
-          className="desktop-nav__link"
-          activeClassName="desktop-nav__link--active"
-        >
-          post feed
-        </NavLink>
-      </div>
-    );
-
-    right = (
-      <div className="desktop-nav__section">
-        <NavLink
-          to="/dashboard"
-          className="desktop-nav__link"
-          activeClassName="desktop-nav__link--active"
-        >
-          dashboard
-        </NavLink>
-        <div className="desktop-nav__logout-container">
-          <img
-            className="desktop-nav__user-image"
-            src={user.photo.path}
-            alt={user.fullname}
-          />
-          <div className="desktop-nav__link" onClick={logoutUser} role="button">
-            logout
+    content = (
+      <>
+        <div>
+          <Link to="/profiles">developers</Link>
+          <Link to="/posts">post feed</Link>
+        </div>
+        <div>
+          <Link to="/dashboard">dashboard</Link>
+          <div>
+            <img src={user.photo.path} alt={user.fullname} />
+            <div onClick={logoutUser} role="button">
+              logout
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 
   return (
     <nav className="desktop-nav" ref={navRef}>
-      {left}
-      {right}
+      {content}
     </nav>
   );
 };

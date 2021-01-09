021468428f1129884e69d2645435313222428131
yosri mhadmi <bavary1515@gmail.com>
2021-01-09 16:40:54 +0100

    removed a costum hook.

--- client/src/components/pages/Landing/Landing.jsx
+++ client/src/components/pages/Landing/Landing.jsx
@@ -2,33 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Landing.scss';
-import useChangeNavBgColor from './useChangeNavBgColor';
 
-const Landing = () => {
-  useChangeNavBgColor();
-
-  return (
-    <div className="landing">
-      <div className="landing__nav">
-        <Link to="/developers" className="landing__profiles-link">
-          Developers
+const Landing = () => (
+  <div className="landing">
+    <div className="landing__nav">
+      <Link to="/developers" className="landing__profiles-link">
+        Developers
+      </Link>
+      <Link to="/login" className="landing__login-button">
+        Log in
+      </Link>
+    </div>
+    <h4 className="landing__outlook">connector</h4>
+    <div className="landing__wrapper">
+      <h1 className="landing__headline">Connect. Ask.</h1>
+      <h1 className="landing__headline">Get valid responses.</h1>
+      <div className="landing__button-wrapper">
+        <h3 className="landing__small-headline">Join our community</h3>
+        <Link to="/signup" className="landing__button">
+          Create account
         </Link>
-        <Link to="/login" className="landing__login-button">
-          Log in
-        </Link>
-      </div>
-      <h4 className="landing__outlook">connector</h4>
-      <div className="landing__wrapper">
-        <h1 className="landing__headline">Connect. Ask.</h1>
-        <h1 className="landing__headline">Get valid responses.</h1>
-        <div className="landing__button-wrapper">
-          <h3 className="landing__small-headline">Join our community</h3>
-          <Link to="/signup" className="landing__button">
-            Create account
-          </Link>
-        </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
+
 export default Landing;

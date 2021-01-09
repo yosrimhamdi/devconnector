ae4f0f72b9a929390052db10b27c7e37ba854766
yosri mhadmi <bavary1515@gmail.com>
2021-01-09 17:50:33 +0100

    responsive landing.

--- client/src/components/pages/Landing/Landing.jsx
+++ client/src/components/pages/Landing/Landing.jsx
@@ -3,28 +3,31 @@ import { Link } from 'react-router-dom';
 
 import './Landing.scss';
 
-const Landing = () => (
-  <div className="landing">
-    <div className="landing__nav">
-      <Link to="/developers" className="landing__profiles-link">
-        Developers
-      </Link>
-      <Link to="/login" className="landing__login-button">
-        Log in
-      </Link>
-    </div>
-    <h4 className="landing__outlook">connector</h4>
-    <div className="landing__wrapper">
-      <h1 className="landing__headline">Connect. Ask.</h1>
-      <h1 className="landing__headline">Get valid responses.</h1>
-      <div className="landing__button-wrapper">
-        <h3 className="landing__small-headline">Join our community</h3>
-        <Link to="/signup" className="landing__button">
-          Create account
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="landing__nav">
+        <Link to="/developers" className="landing__profiles-link">
+          Developers
+        </Link>
+        <Link to="/login" className="landing__login-button">
+          Log in
         </Link>
       </div>
+      <h4 className="landing__connector">connector</h4>
+      <div className="landing__cta-wrapper">
+        <h1 className="landing__headline">connect.</h1>
+        <div className="landing__headline">crganize.</div>
+        <h1 className="landing__headline">get responses.</h1>
+        <div className="landing__button-wrapper">
+          <h3 className="landing__message">join our community</h3>
+          <Link to="/signup" className="landing__button">
+            Create account
+          </Link>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Landing;

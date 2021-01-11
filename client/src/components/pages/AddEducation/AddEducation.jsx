b4bd4a42cb64341e7b7012403781984bb85f5426
yosri mhadmi <bavary1515@gmail.com>
2021-01-11 13:09:12 +0100

    changed other google-form layout.

--- client/src/components/pages/AddEducation/AddEducation.jsx
+++ client/src/components/pages/AddEducation/AddEducation.jsx
@@ -20,49 +20,51 @@ const AddEducation = ({ handleSubmit, addEducation, loading }) => {
     <>
       <form onSubmit={handleSubmit(addEducation)} className="add-education">
         <BackSection title="education" />
-        <div className="add-education__content-wrapper">
-          <h1 className="add-education__title">add education</h1>
-          <Field
-            name="school"
-            placeholder="school"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="degree"
-            placeholder="degree or certification"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="fieldOfStudy"
-            placeholder="field of study**"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="from"
-            type="date"
-            component={Input}
-            description="from date**"
-          />
-          <Field
-            name="to"
-            type="date"
-            component={Input}
-            description="to date"
-          />
-          <Field
-            name="description"
-            placeholder="program description"
-            description="tell us about the program that you were in"
-            component={TextArea}
-          />
-          <div className="add-experience__button-wrapper">
-            <Link to="/dashboard" text="cancel" transparent rounded />
-            <Button text="submit" rounded />
+        <div className="add-education__container">
+          <div className="add-education__content-wrapper">
+            <h1 className="add-education__title">add education</h1>
+            <Field
+              name="school"
+              placeholder="school"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="degree"
+              placeholder="degree or certification"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="fieldOfStudy"
+              placeholder="field of study**"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="from"
+              type="date"
+              component={Input}
+              description="from date**"
+            />
+            <Field
+              name="to"
+              type="date"
+              component={Input}
+              description="to date"
+            />
+            <Field
+              name="description"
+              placeholder="program description"
+              description="tell us about the program that you were in"
+              component={TextArea}
+            />
+            <div className="add-experience__button-wrapper">
+              <Link to="/dashboard" text="cancel" transparent rounded />
+              <Button text="submit" rounded />
+            </div>
+            <Spinner visible={loading} onBottom />
           </div>
-          <Spinner visible={loading} onBottom />
         </div>
       </form>
     </>
--- client/src/components/pages/AddExperience/AddExperience.jsx
+++ client/src/components/pages/AddExperience/AddExperience.jsx
@@ -20,49 +20,51 @@ const AddExperience = ({ handleSubmit, addExperience, loading }) => {
     <>
       <form onSubmit={handleSubmit(addExperience)} className="add-experience">
         <BackSection title="experience" />
-        <div className="add-experience__content-wrapper">
-          <h1 className="add-experience__title">add experience</h1>
-          <Field
-            name="company"
-            placeholder="company**"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="title"
-            placeholder="job title**"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="location"
-            placeholder="location"
-            component={Input}
-            marginBottom="2.5em"
-          />
-          <Field
-            name="from"
-            type="date"
-            component={Input}
-            description="from date**"
-          />
-          <Field
-            name="to"
-            type="date"
-            component={Input}
-            description="to date"
-          />
-          <Field
-            name="description"
-            placeholder="job description"
-            description="tell us about the position"
-            component={TextArea}
-          />
-          <div className="add-experience__button-wrapper">
-            <Link to="/dashboard" text="cancel" transparent rounded />
-            <Button text="submit" rounded />
+        <div className="add-experience__container">
+          <div className="add-experience__content-wrapper">
+            <h1 className="add-experience__title">add experience</h1>
+            <Field
+              name="company"
+              placeholder="company**"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="title"
+              placeholder="job title**"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="location"
+              placeholder="location"
+              component={Input}
+              marginBottom="2.5em"
+            />
+            <Field
+              name="from"
+              type="date"
+              component={Input}
+              description="from date**"
+            />
+            <Field
+              name="to"
+              type="date"
+              component={Input}
+              description="to date"
+            />
+            <Field
+              name="description"
+              placeholder="job description"
+              description="tell us about the position"
+              component={TextArea}
+            />
+            <div className="add-experience__button-wrapper">
+              <Link to="/dashboard" text="cancel" transparent rounded />
+              <Button text="submit" rounded />
+            </div>
+            <Spinner visible={loading} onBottom />
           </div>
-          <Spinner visible={loading} onBottom />
         </div>
       </form>
     </>

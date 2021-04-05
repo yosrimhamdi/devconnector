e963e677c9d4f66d81293f4d088fec0531fa4f03
yosrimhamdi <bavary1515@gmail.com>
2021-04-05 22:27:34 +0100

    allow cors from portfolio.

--- view/sendReactApp.js
+++ view/sendReactApp.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
 module.exports = (req, res) => {
+  res.set('Access-Control-Allow-Origin', 'https://yosrimhamdi.netlify.app/');
+
   res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 };

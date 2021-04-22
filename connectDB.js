84db5f8ef99d3cae4fb59b82a64eb409369afafb
yosrimhamdi <bavary1515@gmail.com>
2021-04-22 11:30:11 +0100

    changed a file name.

--- connectDB.js
+++ connectDB.js
@@ -4,7 +4,7 @@ const { DB_URI, DB_USER_PASSWORD } = process.env;
 
 const URI = DB_URI.replace('<password>', DB_USER_PASSWORD);
 
-module.exports = async () => {
+(async () => {
   try {
     await mongoose.connect(URI, {
       useNewUrlParser: true,
@@ -17,4 +17,4 @@ module.exports = async () => {
   } catch (err) {
     console.error(err);
   }
-};
+})();
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
-const connect = require('./connect');
+require('./connectDB');
 
 const app = require('./app');
 
 const { PORT } = process.env;
-
-connect();
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
